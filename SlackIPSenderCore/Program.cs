bad716a8ba16f5ad117fa283359a3cc41d149492
André Leblanc <aleblanc@lallemand.com>
2019-01-03 10:24:05 -0500

    Add project files.

---
+++ SlackIPSenderCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlackIPSenderCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
