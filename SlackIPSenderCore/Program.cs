c3e8e5fc6249437b68277c001159a76cccc13683
André Leblanc <aleblanc@lallemand.com>
2019-01-05 10:44:39 -0500

    docker test

--- SlackIPSenderCore/Program.cs
+++ SlackIPSenderCore/Program.cs
@@ -1,48 +1,66 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
 using System.Net;
-using System.Threading;
-using ServiceStack;
-using ServiceStack.Logging;
-using ServiceStack.Logging.Slack;
-using ServiceStack.Text;
+using System.Net.NetworkInformation;
 
 namespace SlackIPSenderCore
 {
     class Program
     {
-        private static ILogFactory logFactory;
-
         static void Main(string[] args)
         {
-
-            InitLog();
-            ILog log = LogManager.GetLogger(typeof(Program));
+            //InitLog();
+            //ILog log = LogManager.GetLogger(typeof(Program));
 
             string hostName = Dns.GetHostName(); // Retrive the Name of HOST
-
             // Get the IP
             var myIPs = Dns.GetHostEntry(hostName).AddressList;
-            var json = myIPs.Select(ip => ip.ToString()).ToJson().IndentJson();
-            log.InfoFormat("Here is my My Last IP Adresses: {0} {1} {0} Delivered On: {2} ", Environment.NewLine, json, DateTime.UtcNow.ToString("R"));
-            Thread.Sleep(1000);
-        }
+            var ips = myIPs.Select(ii => ii.ToString());
 
-        private static void InitLog()
-        {
-                                                          
-            LogManager.LogFactory =  new SlackLogFactory("https://hooks.slack.com/services/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", debugEnabled: false)
+
+            var nics = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var adapter in nics)
             {
-                //Alternate default channel than one specified when creating Incoming Webhook.
-                DefaultChannel = "#notification-log",
-                //Custom channel for Fatal logs. Warn, Info etc will fallback to DefaultChannel or 
-                //channel specified when Incoming Webhook was created.
-                //FatalChannel = "#fatal-log",
-                //Custom bot username other than default
-                //BotUsername = "Guybrush Threepwood",
-                //Custom channel prefix can be provided to help filter logs from different users or environments. 
-                ChannelPrefix = "RPi"
-            };
+                var physicalAddress = adapter.GetPhysicalAddress();
+                var description = adapter.Description;
+               Console.WriteLine(physicalAddress + "   " + description);
+            }
+            var properties = IPGlobalProperties.GetIPGlobalProperties();
+            var connections = properties.GetActiveTcpConnections();
+            foreach (TcpConnectionInformation information in connections)
+            {
+                var localEndPoint = information.LocalEndPoint;
+                Console.WriteLine(localEndPoint);
+            }
+
+            //curl - X POST - H 'Content-type: application/json'--data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+
+            string ip = String.Empty;
+            foreach (var item in ips.ToList())
+            {
+                ip = ip + " -> " + item;
+            }
+
+            foreach (IPAddress ipAddress in myIPs)
+            {
+                if (ipAddress.AddressFamily.ToString() == "InterNetwork")
+                {
+                    ip = ip + " ( " + ipAddress + " ) ";
+                }
+            }
+
+
+
+
+            string webhookUrl = @"https://hooks.slack.com/services/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";
+            string text = string.Format("Here is my My Last IP Adresses: {0} {1} {0} Delivered On: {2} ", Environment.NewLine, ip, DateTime.UtcNow.ToString("R"));
+            string channelName = "#notification-log";
+            Slack.Net.Message message = new Slack.Net.Message(webhookUrl, text, channelName);
+            //message.Send();
+            Console.WriteLine(text);
+            Console.ReadKey();
+            //Thread.Sleep(1000);
         }
     }
 }
